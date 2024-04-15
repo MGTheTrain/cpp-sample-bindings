0d173af5cbc0ce2145d5847f50935bff5ecdab5f
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-15 16:10:48 +0200

    add cmake modules

---
+++ cmake/CTestConfig.cmake
@@ -0,0 +1,10 @@
+include(CTest)
+enable_testing()
+
+set(CTEST_PROJECT_NAME "${PROJECT_NAME}")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "github.com/")
+set(CTEST_DROP_LOCATION "mgtt/cpp-sample-bindings")
+set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_DROP_SITE_CDASH_BI TRUE)
