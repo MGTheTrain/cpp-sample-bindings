3def80e64ecfeb5b5dc434260f7579e54c79d46f
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-17 20:15:04 +0200

    copy assets to correct dir trough CMakeLists.txt and provide correct path for loadAudioFile(...) function

--- modules/audio/test/audio-test.cpp
+++ modules/audio/test/audio-test.cpp
@@ -35,14 +35,14 @@ class AudioPlaybackTest : public ::testing::Test {
 
 // Test case for loading an audio file
 TEST_F(AudioPlaybackTest, LoadAudioFileTest) {
-  const char *filename = "path/to/your/audio/file.mp3";
+  const char *filename = "assets/mp3/file_example_MP3_700KB.mp3";
   AudioData audioData;
   EXPECT_TRUE(loadAudioFile(filename, &audioData));
 }
 
 // Test case for starting audio playback
 TEST_F(AudioPlaybackTest, StartPlaybackTest) {
-  const char *filename = "path/to/your/audio/file.mp3";
+  const char *filename = "assets/mp3/file_example_MP3_700KB.mp3";
   AudioData audioData;
   ASSERT_TRUE(loadAudioFile(filename, &audioData));
   EXPECT_TRUE(startPlayback(&audioData));
@@ -50,7 +50,7 @@ TEST_F(AudioPlaybackTest, StartPlaybackTest) {
 
 // Test case for closing audio file and terminating PortAudio
 TEST_F(AudioPlaybackTest, CloseAudioFileTest) {
-  const char *filename = "path/to/your/audio/file.mp3";
+  const char *filename = "assets/mp3/file_example_MP3_700KB.mp3";
   AudioData audioData;
   ASSERT_TRUE(loadAudioFile(filename, &audioData));
   ASSERT_TRUE(startPlayback(&audioData));
