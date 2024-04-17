71f5ae64c71bde1626b08654a6581761a8408749
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-17 19:22:00 +0200

    rename folder and adhere Google style guideline in audio-player.cpp

--- apps/audio-player/src/audio-player.cpp
+++ apps/audio-player/src/audio-player.cpp
@@ -26,13 +26,11 @@
 int main(int argc, char *argv[]) {
     const char * path = "assets/mp3/file_example_MP3_700KB.mp3"; 
     AudioData audioData;
-    if (!loadAudioFile(path, &audioData))
-    {
+    if (!loadAudioFile(path, &audioData)) {
         return 1;
     }
 
-    if (!startPlayback(&audioData))
-    {
+    if (!startPlayback(&audioData)) {
         closeAudioFile(&audioData);
         return 1;
     }
