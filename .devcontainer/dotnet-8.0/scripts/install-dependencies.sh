efae7d8e32bab12a9e7741381a26d5722fbe58e0
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-18 23:55:03 +0200

    add missing apt dependencies required in combination with sdl2 vcpkg

--- .devcontainer/dotnet-8.0/scripts/install-dependencies.sh
+++ .devcontainer/dotnet-8.0/scripts/install-dependencies.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 apt-get update
-apt-get install -y cmake python3 python3-pip pkg-config portaudio19-dev nasm
+apt-get install -y cmake python3 python3-pip pkg-config portaudio19-dev nasm libwayland-dev libxkbcommon-dev libegl1-mesa-dev
 #pip3 install clang-format cpplint
 pip3 install jinja2
 
